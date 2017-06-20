4718f4f1180e6fc107afff63f9285df3c80e8183
Márton Makrai <makrai.nlp@gmail.com>
2017-06-20 15:23:34 +0200

    word frequency ranking is taken from seed dictionary

--- multisense_translate.py
+++ multisense_translate.py
@@ -1,18 +1,18 @@
 # coding: utf-8
 
 from collections import defaultdict
+from functools import reduce
 import logging
 import os
 import sys
 
 import numpy as np
-from scipy.spatial.distance import pdist
 from sklearn.linear_model import LinearRegression
 from gensim.models import KeyedVectors
 
 logging.basicConfig(
     format="%(asctime)s %(module)s (%(lineno)s) %(levelname)s %(message)s",
-    level=logging.INFO)
+    level=logging.DEBUG)
 
 class MultiSenseLinearTranslator():
     def __init__(self,
@@ -47,111 +47,88 @@ class MultiSenseLinearTranslator():
                 return embed
 
     def main(self):
-        with open(self.seed_filen) as self.seed_f:
-            train_size = 0
-            while train_size < self.train_size:
-                tg, sr = self.seed_f.readline().strip().split()
-                if sr in self.source_firsts and tg in self.target_embed:
-                    self.train_sr[train_size] = self.source_firsts[sr]
-                    self.train_tg[train_size] = self.target_embed[tg]
-                    train_size += 1
-
-            logging.info('trained on {} words'.format(len(self.train_sr)))
-            self.regression = LinearRegression(n_jobs=-2)
-            self.regression.fit(self.train_sr, self.train_tg)
+            self.train()
             self.test()
 
-    def translate_one_vector(self, tg_vecs):
-        ambig_neighbors = set()
-        for tg_vec in tg_vecs:
-            sense_neighbors, _ = zip(*self.target_embed.similar_by_vector(
-                tg_vec, restrict_vocab=self.restrict_vocab))
-            ambig_neighbors |= set(sense_neighbors)
-        return ambig_neighbors
-
-    def read_test_dict(self):
-        test_dict = defaultdict(set)
-        for line in self.seed_f.readlines():
-            tg, sr = line.split()
-            test_dict[sr].add(tg)
-        return test_dict 
+    def train(self):
+        train_size = 0
+        while train_size < self.train_size:
+            tg, sr = self.seed_f.readline().strip().split()
+            if sr in self.source_firsts and tg in self.target_embed:
+                self.train_sr[train_size] = self.source_firsts[sr]
+                self.train_tg[train_size] = self.target_embed[tg]
+                train_size += 1 
+        logging.info('trained on {} words'.format(len(self.train_sr)))
+        self.regression = LinearRegression(n_jobs=-2)
+        self.regression.fit(self.train_sr, self.train_tg)
 
     def test(self):
-        test_dict = self.read_test_dict()
+        self.test_dict = self.read_test_dict()
         with open(self.source_mse_filen) as source_mse:
             logging.info(
                 'skipping header: {}'.format(source_mse.readline().strip()))
-            test_size = 0
-            score_at10 = 0
-            good_ambig = 0
+            self.test_size = 0
+            self.score_at10 = 0
+            self.good_ambig = 0
             sr_word = ''
             sr_vecs = []
-            good_trans = set()
-            while True:#test_size < self.test_size:
+            while True:#self.test_size < self.test_size:
                 line = source_mse.readline()
                 if not line:
                     logging.warning(
-                        'tested only on {} items'.format(test_size))
+                        'tested only on {} items'.format(self.test_size))
                     break
                 new_sr_word, vect_str = line.strip().split(maxsplit=1)
                 if new_sr_word != sr_word:
-                    if sr_word in test_dict:
-                        self.log_prec(score_at10, test_size, good_ambig) 
-                        test_size += 1
-                        sr_vecs = np.concatenate(sr_vecs)
-                        tg_vecs = sr_vecs.dot(self.regression.coef_.T)
-                        ambig_neighbors = self.translate_one_vector(tg_vecs)
-                        good_trans = list(
-                            test_dict[sr_word].intersection(ambig_neighbors))
-                        if good_trans:
-                            score_at10 += 1
-                        if len(good_trans) > 1:
-                            good_ambig += 1
-                            good_vecs = np.concatenate([
-                                self.target_embed[w].reshape((1,-1))
-                                for w in good_trans])
-                            if len(good_vecs) > 2:
-                                sim_mx = good_vecs.dot(good_vecs.T)
-                                ij = np.unravel_index(
-                                    np.argmin(sim_mx), sim_mx.shape)
-                                tgw1, tgw2 = [good_trans[i] for i in ij]
-                                logging.debug((sr_word, good_trans, tgw1,
-                                               tgw2))
-                            else:
-                                tgw1, tgw2 = good_trans
-                            print('\t'.join(['{}'.format(
-                                self.target_embed.similarity(tgw1, tgw2)),
-                                sr_word, tgw1, tgw2]))
+                    if sr_word in self.test_dict:
+                        self.eval_word(sr_word, sr_vecs)
                     sr_word = new_sr_word
                     sr_vecs = []
                 sr_vecs.append(np.fromstring(vect_str, sep=' ').reshape((1,-1)))
 
-    def log_prec(self, score_at10, test_size, good_ambig):
-        if not test_size % 1000 and test_size:
-            logging.info(
-                'prec after testing on {} words: {:%}, good_ambig: {}'.format(
-                    test_size, float(score_at10)/test_size, good_ambig))
-
-    def test_baseline(self):
-        test_size = 0
-        score_at10 = 0
-        while test_size < self.test_size:
-            line = self.seed_f.readline().strip()
-            try:
+    def read_test_dict(self):
+        with open(self.seed_filen) as seed_f:
+            self.test_dict = defaultdict(set)
+            for line in seed_f.readlines():
                 tg, sr = line.split()
-            except:
-                logging.err(line)
-            if sr in self.source_firsts:
-                tg_vec = self.regression.coef_.dot(self.source_firsts[sr])
-                tg_10, _ = zip(*self.target_embed.similar_by_vector(tg_vec,
-                                                                       restrict_vocab=self.restrict_vocab))
-                test_size += 1
-                if tg in tg_10:
-                    score_at10 += 1 
-        prec_at10 = float(score_at10)/test_size
-        logging.info('final prec@10 is {:%} on {} items'.format(prec_at10, test_size))
-        return prec_at10
+                self.test_dict[sr].add(tg)
+            return self.test_dict 
+
+    def eval_word(self, sr_word, sr_vecs): 
+        self.log_prec() 
+        self.test_size += 1
+        tg_vecs = np.concatenate(sr_vecs).dot(self.regression.coef_.T)
+        word_neighbors = [self.neighbors_by_vector(v) for v in tg_vecs]
+        good_trans = [ns.intersection(self.test_dict[sr_word]) 
+                      for ns in word_neighbors]
+        all_good_trans = reduce(set.union, good_trans, set())
+        if all_good_trans:
+            self.score_at10 += 1 
+            if len(all_good_trans) > 1:
+                self.good_ambig += 1
+                good_ambig_trans = [s for s in good_trans if s]
+                if len(good_ambig_trans) > 1:
+                    logging.debug((sr_word, good_ambig_trans))
+    
+    def neighbors_by_vector(self, vect):
+        sense_neighbors, _ = zip(*self.target_embed.similar_by_vector( 
+            vect, restrict_vocab=self.restrict_vocab))
+        return set(sense_neighbors)
+
+    def log_prec(self):
+        if not self.test_size % 1000 and self.test_size:
+            logging.info(
+                'prec after testing on {} words: {:%}, self.good_ambig: {}'.format(
+                    self.test_size, float(self.score_at10)/self.test_size, self.good_ambig))
 
 if __name__ == '__main__':
     MultiSenseLinearTranslator(#target_embed=sys.argv[1]
                               ).main()
+
+""" 
+if len(good_vecs) > 2:
+    sim_mx = good_vecs.dot(good_vecs.T)
+    ij = np.unravel_index( np.argmin(sim_mx), sim_mx.shape)
+    tgw1, tgw2 = [good_trans[i] for i in ij]
+    logging.debug((sr_word, good_trans, tgw1, tgw2))
+"""
