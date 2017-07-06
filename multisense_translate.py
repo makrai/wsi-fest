31a7d441ec71db63df4b55723842ac98581a1579
makrai <makraimarton@gmail.com>
2017-07-06 16:53:54 +0200

    centralize is default

--- multisense_translate.py
+++ multisense_translate.py
@@ -23,7 +23,7 @@ class MultiSenseLinearTranslator():
     def __init__(self, args=None, source_mse=None, target_embed=None,
                  seed_dict=None, orthog=False, translate_all=False, reverse=True,
                  restrict_vocab=2**15, prec_level=10, normalize=False,
-                 centralize=False):
+                 centralize=True):
 
         def get_first_vectors(filen):
             root, ext = os.path.splitext(filen)
@@ -70,14 +70,14 @@ class MultiSenseLinearTranslator():
         self.target_embed = get_first_vectors(self.args.target_embed)
 
     def main(self):
-        if self.args.normalize:
-            self.normalize(self.source_firsts.syn0)
-            self.normalize(self.target_embed.syn0)
         if self.args.centralize:
             self.sr_center = self.get_center(self.source_firsts.syn0)
             self.tg_center = self.get_center(self.target_embed.syn0)
             self.source_firsts.syn0 -= self.sr_center
             self.target_embed.syn0 -= self.tg_center 
+        if self.args.normalize:
+            self.normalize(self.source_firsts.syn0)
+            self.normalize(self.target_embed.syn0)
         with open(self.args.seed_dict) as self.seed_f:
             self.train()
             return self.test()
@@ -206,10 +206,10 @@ class MultiSenseLinearTranslator():
                 self.args.source_mse, skip_header=1, max_rows=self.args.restrict_vocab,
                 usecols=np.arange(1, int(dim)+1), dtype='float16',
                 comments=None)
-            if self.args.normalize:
-                self.normalize(source_mse)
             if self.args.centralize: 
                 source_mse -= self.sr_center
+            if self.args.normalize:
+                self.normalize(source_mse)
             logging.info(
                 'Source vocab and mx read {}'.format(source_mse.shape))
             return sr_vocab, source_mse
@@ -284,10 +284,10 @@ class MultiSenseLinearTranslator():
                             if len(sr_vecs) > 1:
                                 self.sys_ambig += 1
                             tg_vecs = np.concatenate(sr_vecs).dot(self.regression.coef_.T)
-                            if self.args.normalize:
-                                self.normalize(tg_vecs)
                             if self.args.centralize: 
                                 tg_vecs -= self.tg_center
+                            if self.args.normalize:
+                                self.normalize(tg_vecs)
                             neighbor_by_vec = [neighbors_by_vector(v) for v in tg_vecs]
                         eval_word(sr_word, neighbor_by_vec)
                     sr_word = new_sr_word
@@ -314,7 +314,8 @@ def parse_args():
     parser.add_argument('--seed_dict')
     parser.add_argument( '--orthog', action='store_true')
     parser.add_argument( '--normalize', action='store_true')
-    parser.add_argument( '--centralize', action='store_true')
+    parser.add_argument( '--no-centralize', dest='centralize',
+                        action='store_false')
     parser.add_argument('--translate_all', action='store_true')
     parser.add_argument(
         '--fwd-nn-search', dest='reverse', action='store_false',
